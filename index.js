1fd56c8b70cfe08dd8db6806efd717b3a9b14eb6
gitkazumaa <kazumaalternate@gmail.com>
2023-06-28 08:01:04 -0700

    Add todoItem function

---
+++ index.js
@@ -0,0 +1,8 @@
+const todoItem = (t, d, dD, p, n, cL) => {
+    this.title = t;
+    this.description = d;
+    this.dueDate = dD;
+    this.priority = p;
+    this.notes = n;
+    this.checkList = cL;
+}
