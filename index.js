93464a8e1332c8e7ce4996e27dfc0257cf3ac280
gitkazumaa <kazumaalternate@gmail.com>
2023-06-29 08:03:00 -0700

    Initialize home and reformat

--- index.js
+++ index.js
@@ -1,3 +1,70 @@
+const project = (projectTitle) => {
+    this.title = projectTitle;
+    return {title}
+}
+
+const projectUI = (project) => {
+    const projectContainer = document.createElement("div");
+    projectContainer.className = "project-container";
+
+    const title = document.createElement("span");
+    title.innerHTML = project.title;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.type = "button";
+    deleteBtn.addEventListener("click", () => {
+        projectContainer.remove();
+    });
+
+    projectContainer.appendChild(title);
+    projectContainer.appendChild(deleteBtn);
+
+    return projectContainer;
+}
+
+const projectForm = document.getElementById("project-form");
+projectForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const [projectSidebar] = document.getElementsByClassName("project-sidebar");
+    const [projectTitle] = document.getElementsByName("project-title");
+    const item = project(projectTitle.value);
+    const itemUI = projectUI(item);
+    projectSidebar.appendChild(itemUI);
+});
+
+const [projectSidebar] = document.getElementsByClassName("project-sidebar");
+projectSidebar.appendChild((() => {
+    const homeProject = document.createElement("div");
+    homeProject.className = "project-container";
+
+    const title = document.createElement("span");
+    title.innerHTML = "Home";
+
+    homeProject.appendChild(title);
+    
+    return homeProject;
+})());
+
+const todoForm = document.getElementById("todo-form");
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const [title] = document.getElementsByName("title");
+    const [description] = document.getElementsByName("description");
+    const [dueDate] = document.getElementsByName("due-date");
+    let priority = document.getElementsByName("priority");
+    for (let i = 0; i < priority.length; i++) {
+        if (priority[i].checked) {
+            priority = priority[i];
+            break;
+        }
+    }
+
+    const [todoContainer] = document.getElementsByClassName("todo-container");
+    const item = todoItem(title.value, description.value, dueDate.value, priority.value);
+    const itemUI = toDoItemUI(item);
+    todoContainer.appendChild(itemUI);
+});
+
 const todoItem = (t, d, dD, p, n, cL) => {
     this.title = t;
     this.description = d;
@@ -30,56 +97,3 @@ const toDoItemUI = (todoItem) => {
 
     return todo;
 }
-
-const project = (projectTitle) => {
-    this.title = projectTitle;
-    return {title}
-}
-
-const projectUI = (project) => {
-    const projectContainer = document.createElement("div");
-
-    const title = document.createElement("span");
-    title.innerHTML = project.title;
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.type = "button";
-    deleteBtn.addEventListener("click", () => {
-        projectContainer.remove();
-    });
-
-    projectContainer.appendChild(title);
-    projectContainer.appendChild(deleteBtn);
-
-    return projectContainer;
-}
-
-const todoForm = document.getElementById("todo-form");
-todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const [title] = document.getElementsByName("title");
-    const [description] = document.getElementsByName("description");
-    const [dueDate] = document.getElementsByName("due-date");
-    let priority = document.getElementsByName("priority");
-    for (let i = 0; i < priority.length; i++) {
-        if (priority[i].checked) {
-            priority = priority[i];
-            break;
-        }
-    }
-
-    const [todoContainer] = document.getElementsByClassName("todo-container");
-    const item = todoItem(title.value, description.value, dueDate.value, priority.value);
-    const itemUI = toDoItemUI(item);
-    todoContainer.appendChild(itemUI);
-});
-
-const projectForm = document.getElementById("project-form");
-projectForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const [projectSidebar] = document.getElementsByClassName("project-sidebar");
-    const [projectTitle] = document.getElementsByName("project-title");
-    const item = project(projectTitle.value);
-    const itemUI = projectUI(item);
-    projectSidebar.appendChild(itemUI)
-})
